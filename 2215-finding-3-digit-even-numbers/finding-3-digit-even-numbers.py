3289aa44c845f6c823dbae34c096881366d580c7
Mantas Taradai <mantastaradai@gmail.com>
2025-05-12 10:42:30 +0300

    Time: 47 ms (68.85%) | Memory: 18.1 MB (28.25%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.py
@@ -1,15 +1,11 @@
 class Solution:
-    def findEvenNumbers(self, digits: List[int]) -> List[int]:
-        freq=Counter(digits)
-        ans=[]
-        for x in range(100, 1000, 2):
-            x0, x1, x2=x%10, (x//10)%10, x//100
-            freq[x0]-=1
-            freq[x1]-=1
-            freq[x2]-=1
-            if freq[x0]>=0 and freq[x1]>=0 and freq[x2]>=0:
-                ans.append(x)
-            freq[x0]+=1
-            freq[x1]+=1
-            freq[x2]+=1
-        return ans
+    def findEvenNumbers(self, digits):
+        from collections import Counter
+        freq = Counter(digits)
+        res = []
+        for num in range(100, 1000, 2):
+            parts = [num // 100, (num // 10) % 10, num % 10]
+            count = Counter(parts)
+            if all(freq[d] >= count[d] for d in count):
+                res.append(num)
+        return res
