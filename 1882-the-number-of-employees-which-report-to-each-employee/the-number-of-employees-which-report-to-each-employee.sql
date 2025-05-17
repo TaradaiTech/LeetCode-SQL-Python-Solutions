067b23c0ce6ff98becb72b764a7b1a7f6c200d04
Mantas Taradai <mantastaradai@gmail.com>
2025-05-17 19:21:25 +0300

    Time: 354 ms (40.78%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,15 +1,14 @@
 select
-    e1.employee_id,
-    e1.name,
-    count(e2.employee_id) as reports_count,
-    round(avg(e2.age)) as average_age
-from
-    employees e1
-join employees e2 on
-    e1.employee_id = e2.reports_to
+    e2.employee_id, 
+    e2.name, 
+    count(e1.reports_to) as reports_count, 
+    round(avg(e1.age)) as average_age
+from employees e1
+join employees e2 on 
+    e2.employee_id = e1.reports_to
 group by
-    e1.employee_id, e1.name
+    e2.employee_id, e2.name
 having
-    count(e2.employee_id) > 0
+    count(e1.reports_to) > 0
 order by
-    e1.employee_id
+    e2.employee_id
