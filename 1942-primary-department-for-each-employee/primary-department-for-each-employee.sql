170d31201e8067019f6117fa90dbb35b18faf176
Mantas Taradai <mantastaradai@gmail.com>
2025-06-02 19:31:33 +0300

    Time: 237 ms (47.80%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,4 +1,7 @@
-select employee_id, department_id
-from Employee
-where primary_flag = 'Y' or employee_id in (
-select employee_id from employee group by employee_id having count(department_id) = 1)
+select
+    employee_id, department_id
+from
+    employee
+where
+    primary_flag = 'Y' OR employee_id in (select employee_id from employee group by employee_id having
+    count(employee_id)=1)
